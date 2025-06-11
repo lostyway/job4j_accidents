9bf3cb7d2409151e443d2783b8f536f9ed66c33a
lostyway <hardsheller@gmail.com>
2025-06-11 21:55:44 +0300

    Вроде бы тесты работают

--- src/main/java/ru/job4j/accidents/config/HbmConfig.java
+++ src/main/java/ru/job4j/accidents/config/HbmConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:db.properties")
+@PropertySource("classpath:application.properties")
 @EnableTransactionManagement
 public class HbmConfig {
 
